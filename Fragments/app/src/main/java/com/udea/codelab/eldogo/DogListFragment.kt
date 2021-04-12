79a91d8d157ebcf8102e214f4bdbbfe20ae6a572
Doly Jimenez <doly.jimenez11@gmail.com>
2021-04-11 19:03:33 -0500

    Adding a Fragment Dynamically

---
+++ Fragments/app/src/main/java/com/udea/codelab/eldogo/DogListFragment.kt
@@ -0,0 +1,92 @@
+package com.udea.codelab.eldogo
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.udea.codelab.eldogo.databinding.RecyclerItemDogModelBinding
+
+
+class DogListFragment : Fragment() {
+
+    private lateinit var imageResIds: IntArray
+    private lateinit var names: Array<String>
+    private lateinit var descriptions: Array<String>
+    private lateinit var urls: Array<String>
+
+    companion object {
+
+        fun newInstance(): DogListFragment {
+            return DogListFragment()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context != null) {
+            // Get dog names and descriptions.
+            val resources = context.resources
+            names = resources.getStringArray(R.array.names)
+            descriptions = resources.getStringArray(R.array.descriptions)
+            urls = resources.getStringArray(R.array.urls)
+
+            // Get dog images.
+            val typedArray = resources.obtainTypedArray(R.array.images)
+            val imageCount = names.size
+            imageResIds = IntArray(imageCount)
+            for (i in 0 until imageCount) {
+                imageResIds[i] = typedArray.getResourceId(i, 0)
+            }
+            typedArray.recycle()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view: View = inflater.inflate(R.layout.fragment_dog_list, container,
+            false)
+        val activity = activity as Context
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        recyclerView.adapter = DogListAdapter(activity)
+        return view
+    }
+
+    internal inner class DogListAdapter(context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+        private val layoutInflater = LayoutInflater.from(context)
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+            val recyclerDogModelBinding =
+                RecyclerItemDogModelBinding.inflate(layoutInflater, viewGroup, false)
+            return ViewHolder(recyclerDogModelBinding.root, recyclerDogModelBinding)
+        }
+
+        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+            val dog = DogModel(imageResIds[position], names[position],
+                descriptions[position], urls[position])
+            viewHolder.setData(dog)
+        }
+
+        override fun getItemCount() = names.size
+    }
+
+    internal inner class ViewHolder constructor(itemView: View,
+                                                private val recyclerItemDogListBinding:
+                                                RecyclerItemDogModelBinding
+    ) :
+        RecyclerView.ViewHolder(itemView) {
+
+        fun setData(dogModel: DogModel) {
+            recyclerItemDogListBinding.dogModel = dogModel
+        }
+    }
+
+}
--- Fragments/app/src/main/java/com/udea/codelab/eldogo/MainActivity.kt
+++ Fragments/app/src/main/java/com/udea/codelab/eldogo/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Es null para confirmar que se ha abierto por primera vez la pantalla
+        //es decir que la actividad no tenga un estado guardado
+        if (savedInstanceState == null) {
+
+            //Como se usa fragmentos de soporte se hace referencia al FragmentManager asi:
+            supportFragmentManager
+                .beginTransaction()
+                    //Se pasa el contenedor de las vistas y se agrean los fragments dinamicamente
+                .add(R.id.root_layout, DogListFragment.newInstance(), "dogList")
+                    //pide al FragmentManager ejecutar la transacción
+                .commit()
+        }
     }
 }
